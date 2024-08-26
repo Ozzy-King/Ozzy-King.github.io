47b52e39b62319bd41984a1b00841ce7745b40c9
Ozzy-King <osbourne.clark14@gmail.com>
2024-08-26 03:01:37 +0100

    added lookup for mulitple languages used in repos might have to remove due to unath 60 request limit on api ;(

--- development/devPageFuncs.js
+++ development/devPageFuncs.js
@@ -4,19 +4,19 @@
 window.onload = async function(){
 	OnloadFunc();
 	
-	const response = await fetch("https://api.github.com/users/Ozzy-King/repos"); 
-	const json = await response.text();
-	const myArr = JSON.parse(json);
+	let response = await fetch("https://api.github.com/users/myles-rockland/repos"); 
+	let json = await response.text();
+	let myArr = JSON.parse(json);
 	console.log(myArr.length);
-	const x = document.getElementsByClassName("mainTable")[0];
+	let x = document.getElementsByClassName("mainTable")[0];
 	
-	for(var i = 0; i < myArr.length; i++){
-		var row = document.createElement("tr");
+	for(let i = 0; i < myArr.length; i++){
+		let row = document.createElement("tr");
 		row.setAttribute("onclick","redirecting('"+myArr[i].html_url+"');");
 		
 		//adds the name
-		var col = document.createElement("td");
-		var node = document.createTextNode(myArr[i].name);
+		let col = document.createElement("td");
+		let node = document.createTextNode(myArr[i].name);
 		col.appendChild(node);
 		col.classList.add("beg");
 		row.appendChild(col);
@@ -28,10 +28,22 @@ window.onload = async function(){
 		row.appendChild(col);
 		
 		//add lauguages
-		col = document.createElement("td");
-		node = document.createTextNode(myArr[i].language);
-		col.appendChild(node);
-		row.appendChild(col);
+		
+		response = await fetch(myArr[i].languages_url); //fetch list of languages
+		json = await response.text();
+		var langArr = JSON.parse(json);
+		langArr = Object.keys(langArr);
+		
+		col = document.createElement("td");//create table data
+		let list = document.createElement("ul");//create uordered list
+		for(let i = 0; i < langArr.length; i++){ //loop thouhg language names
+			let listItem = document.createElement("li");//crate list item
+			let node = document.createTextNode(langArr[i]);//give list items lang name
+			listItem.appendChild(node);
+			list.appendChild(listItem);//add list item to list
+		}
+		col.appendChild(list);//apend list to table data
+		row.appendChild(col);//append table data
 		
 		//add number of forks
 		col = document.createElement("td");
