a32ebbd7284cbfa42081a3cce8c79ab3aec11250
(s) Osbourne Clark <osbourne.clark@students.plymouth.ac.uk>
2025-04-03 17:12:39 +0100

    added more to cert thing

--- newWeb/loadCert.js
+++ newWeb/loadCert.js
@@ -14,4 +14,31 @@ async function loadCert(){
 	var certJSON = JSON.parse(certText);
 	console.log(certJSON);
 	
+	for(let i = 0; i  < certJSON.length; i++){
+		var row = document.createElement('div');
+		
+		var issuerLogo = document.createElement('img');
+		issuerLogo.src = "logo/"+certJSON[i].Issuer.toLowerCase()+".png";
+		row.appendChild(issuerLogo);
+		
+		var CertName = document.createElement('h3');
+		issuerLogo.innerText = "logo/"+certJSON[i].title+".png";
+		row.appendChild(CertName);
+		
+		
+		var infoholder = document.createElement('div');
+		var info = document.createElement('p');
+		info.innerText = "issued: "+certJSON[i].issued;
+		infoholder.appendChild(info);
+		
+		info = document.createElement('p');
+		info.innerText = "expires: "+certJSON[i].expire;
+		infoholder.appendChild(info);
+		
+		row.appendChild(infoholder);
+		
+		contentEle.appendChild(row);
+	}
+	issueLogo.src = "logo/"
+	
 }
--- newWeb/loadRepoInfo.js
+++ newWeb/loadRepoInfo.js
@@ -1,4 +1,4 @@
-var PushDates = [];
+ = [];
 //contains
 //{
 //	name: String
